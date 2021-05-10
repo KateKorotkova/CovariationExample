126a8b29fb006307b9f80f6b5baee2a221ddc7ba
Ekaterina Korotkova <e.korotkova.2013@yandex.ru>
2021-05-10 15:50:13 +0300

    Добавлен проект

---
+++ CovariationExample/Entities.cs
@@ -0,0 +1,112 @@
+﻿namespace CovariationExample
+{
+
+    #region Предметная область
+
+    public class SmartPhone
+    {
+        public virtual string Name => "Just a SmartPhone";
+    }
+
+    public class Galaxy : SmartPhone
+    {
+        public override string Name => "Galaxy";
+
+        public void CreateGoogleAccount()
+        {
+
+        }
+
+        public void CreateVisualPassword()
+        {
+
+        }
+    }
+
+
+    public class IPhone : SmartPhone
+    {
+        public override string Name => "IPhone";
+
+        public void ConnectToAppleId()
+        {
+
+        }
+
+        public void ConnectToiCloud()
+        {
+
+        }
+
+        public void TuneFaceId()
+        {
+
+        }
+    }
+
+    #endregion
+
+
+
+    #region Фабрики
+
+    public class UserWishers
+    {
+       
+    }
+
+    public interface ISmartPhoneCreator<out T>
+    {
+        T CreateSmartPhone(UserWishers userWishers);
+    }
+
+    public class GalaxyCreator : ISmartPhoneCreator<Galaxy>
+    {
+        public Galaxy CreateSmartPhone(UserWishers userWishers)
+        {
+            var galaxy = new Galaxy();
+
+            galaxy.CreateGoogleAccount();
+            galaxy.CreateVisualPassword();
+            
+            return galaxy;
+        }
+    }
+
+    public class IPhoneCreator : ISmartPhoneCreator<IPhone>
+    {
+        public IPhone CreateSmartPhone(UserWishers userWishers)
+        {
+            var iphone = new IPhone();
+
+            iphone.ConnectToAppleId();
+            iphone.ConnectToiCloud();
+            iphone.TuneFaceId();
+
+            return iphone;
+        }
+    }
+
+    #endregion
+
+
+
+
+    public interface ICarDisplayer<in T>
+    {
+        void Display(T car);
+    }
+
+
+
+  
+
+    public class CarNameDisplayer
+    {
+        public void DisplayName(ICarDisplayer<IPhone> carDisplayer)
+        {
+            //carDisplayer.Display(new IPhone());
+            //carDisplayer.Display(new SmartPhone());
+        }
+    }
+}
---
+++ CovariationExample/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CovariationExample
+{
+    class Program
+    {
+        public class MagazineManager
+        {
+            public List<SmartPhone> SmartPhones { get; set; }
+
+            public MagazineManager()
+            {
+                SmartPhones = new List<SmartPhone>();
+            }
+
+            public void CreateSmartPhone(ISmartPhoneCreator<SmartPhone> smartPhoneCreator)
+            {
+                var phone = smartPhoneCreator.CreateSmartPhone(new UserWishers());
+                SmartPhones.Add(phone);
+                
+                FillDocuments();
+                SendRequestToWareHouse();
+            }
+
+            public void ShowAllSmartPhones()
+            {
+                SmartPhones.ForEach(x => Console.WriteLine(x.Name));
+            }
+
+            private void SendRequestToWareHouse()
+            {
+
+            }
+
+            private void FillDocuments()
+            {
+
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            var manager = new MagazineManager();
+
+            var galaxyCreator = new GalaxyCreator();
+            var iPhoneCreator = new IPhoneCreator();
+            
+            manager.CreateSmartPhone(galaxyCreator);
+            manager.CreateSmartPhone(iPhoneCreator);
+            
+            manager.ShowAllSmartPhones();
+        }
+    }
+}
